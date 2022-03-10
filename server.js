da4b1e8db08bea80827883a0e017251b55847d60
fab-ryan <royalfabrice1234@gmail>
2022-03-10 09:18:47 +0200

    logout for auth

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import app from "./src/index.js";
-
-app.listen(3000, () => {
-  console.log("server is running.. 🔥🔥🔥");
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT;
+app.listen(port || 4000, () => {
+  console.log(`server is running.. 🔥🔥🔥 ${port}`);
 });

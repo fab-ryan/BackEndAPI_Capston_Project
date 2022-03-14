3faa056e7f80ee09ddb916ea24ff8398f4855298
fab-ryan <royalfabrice1234@gmail>
2022-03-14 13:27:09 +0200

    default router

--- src/router/router.js
+++ src/router/router.js
@@ -4,12 +4,18 @@ import loginRouter from "./loginRouter.js";
 import commentRouter from "./commentRouter.js";
 import blogRouter from "./blogRouter.js";
 import messageRouter from "./messageRouter.js";
-import subscribeRouter from './subscriberRouter.js'
+import subscribeRouter from "./subscriberRouter.js";
 const routers = express();
 routers.use("/api/v1/", userRouter);
 routers.use("/api/v1/", loginRouter);
 routers.use("/api/v1", commentRouter);
 routers.use("/api/v1/", blogRouter);
-routers.use('/api/v1',subscribeRouter)
+routers.use("/api/v1", subscribeRouter);
 routers.use("/api/v1/", messageRouter);
+routers.get("/", (req, res) => {
+  res.send("Welcome To my Api Capston Project👋🏽👋🏽");
+});
+routers.get("*", (req, res) => {
+  res.status(404).send("No path Found");
+});
 export default routers;

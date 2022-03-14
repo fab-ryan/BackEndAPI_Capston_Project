698c4ff5d9520e835fff5580867eabc946fe98ec
fab-ryan <royalfabrice1234@gmail>
2022-03-14 12:26:00 +0200

    correction on mongoose

--- src/database/databaseMongoose.js
+++ src/database/databaseMongoose.js
@@ -3,5 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const dbconnect = mongoose
   .connect(process.env.MONG_URL)
-  .then(()=>console.log("db connected 🌐"));
+  .then(()=>{console.log("db connected 🌐")}).catch((e)=>
+  {
+    console.log("MongDB connection error",e.message)
+  });
 export { dbconnect as default };

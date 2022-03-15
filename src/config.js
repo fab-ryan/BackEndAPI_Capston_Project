3a5f33e6ba0a3ee54d4ac8f5671ff7618bd66a6c
fab-ryan <royalfabrice1234@gmail>
2022-03-15 18:11:32 +0200

    production envi

--- src/config.js
+++ src/config.js
@@ -3,13 +3,19 @@ dontenv.config();
 const config = {
   development: {
     port: process.env.PORT,
-    db: process.env.MONG_URL,
+    db: process.env.DEV_DB_URL,
     secret: process.env.PRIVATE_KEY,
     mail: process.env.API_GRID_API,
   },
   test: {
     port: process.env.TEST_PORT,
-    db: process.env.MONG_URL_TEST,
+    db: process.env.TEST_URL_TEST,
+    secret: process.env.PRIVATE_KEY,
+    mail: process.env.API_GRID_API,
+  },
+  production: {
+    port: process.env.PROD_PORT,
+    db: process.env.PROD_DB_URL,
     secret: process.env.PRIVATE_KEY,
     mail: process.env.API_GRID_API,
   },

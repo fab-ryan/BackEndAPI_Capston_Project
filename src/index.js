7e7663d41086fa3f2f4a2f85261d0c6212dced5b
fab-ryan <royalfabrice1234@gmail>
2022-03-15 14:32:19 +0200

    finish up documentation

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import cors from "cors";
 const app = express();
 dbconnect;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(
   "/api-docs",
   swaggerUi.serve,
--- src/router/messageRouter.js
+++ src/router/messageRouter.js
@@ -12,15 +12,10 @@ import { verifyToken, IsAdmin } from "../middleware/is_auth.js";
 const router = express.Router();
 
 router.get("/messages", verifyToken, IsAdmin, getAllMessage);
-router.post("/messages", verifyToken, messageValidator, postAllMessage);
-router.get("/singlemessage/:id", verifyToken, IsAdmin, getAMessage);
-router.patch(
-  "/updatemessage/:id",
-  verifyToken,
-  messageValidator,
-  updateMessage
-);
-router.delete("/deletemessage/:id", verifyToken, IsAdmin, deleteMessage);
+router.post("/message", verifyToken, messageValidator, postAllMessage);
+router.get("/message/:id", verifyToken, IsAdmin, getAMessage);
+router.patch("/message/:id", verifyToken, messageValidator, updateMessage);
+router.delete("/message/:id", verifyToken, IsAdmin, deleteMessage);
 
 // Router for Blog
 export default router;
